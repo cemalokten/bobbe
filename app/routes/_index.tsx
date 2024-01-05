506d1f876c8c4e4372116e62116d6505d7dda4e1
cemalokten <cemalokten@gmail.com>
2024-01-05 10:06:24 +0000

    add: template files

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type { UserPayload } from "@prisma/client";
+
+import { useLoaderData } from "@remix-run/react";
+
+import prisma from "~/services/db";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,34 +12,21 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const id = "clqzhivij0000r0j5pl7vyw73";
+  const data = await prisma.user.findUnique({ where: { id } });
+  console.log(data);
+  return data;
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+    <div>
+      <h1 className="text-blue-600 text-8xl">Welcomes to Remix</h1>
+      <ul className="text-red-500">
+        <li>15m Quickstart Blog Tutorial {data?.email}</li>
       </ul>
     </div>
   );
